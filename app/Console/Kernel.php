b47e04e34e17f41c5d6cbf6775213097ab7489fd
bishal221973 <bishalcodeslaravel@gmail.com>
2026-01-19 15:16:53 +0545

    Wrp

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use App\Models\NotificationSetting;
 use App\Models\OrganizationSetting;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
@@ -22,14 +23,22 @@ class Kernel extends ConsoleKernel
         $time = OrganizationSetting::where('key', 'auto_notify')
             ->value('value'); // e.g. "15:01"
 
+        $upcomming = NotificationSetting::where('notification', "upcoming_due_mail")->first();
+        $overdues_mail = NotificationSetting::where('notification', "overdues_mail")->first();
         if ($time) {
-            $schedule->command('app:send-upcomming-dues-mail')
-                ->dailyAt($time)
-                ->withoutOverlapping();
+            if ($upcomming->status == true) {
 
-            $schedule->command('app:send-over-dues-mail')
-                ->dailyAt($time)
-                ->withoutOverlapping();
+                $schedule->command('app:send-upcomming-dues-mail')
+                    ->dailyAt($time)
+                    ->withoutOverlapping();
+            }
+
+            if ($overdues_mail->status == true) {
+
+                $schedule->command('app:send-over-dues-mail')
+                    ->dailyAt($time)
+                    ->withoutOverlapping();
+            }
         }
     }
 
@@ -38,7 +47,7 @@ class Kernel extends ConsoleKernel
      */
     protected function commands(): void
     {
-        $this->load(__DIR__.'/Commands');
+        $this->load(__DIR__ . '/Commands');
 
         require base_path('routes/console.php');
     }
