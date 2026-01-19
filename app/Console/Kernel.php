b4fa3991969eba8e1abdaa73ea2a87a205f86b5a
bishal221973 <bishalcodeslaravel@gmail.com>
2026-01-19 15:07:28 +0545

    Wrp

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use App\Models\OrganizationSetting;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -13,9 +14,19 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('inspire')->hourly();
-        // $schedule->command('app:send-upcomming-dues-mail')->dailyAt('10:00');
+        // $schedule->command('app:send-upcomming-dues-mail')->dailyAt('15:04');
 
-        $schedule->command('app:send-upcomming-dues-mail')->everyMinute();
+        // $schedule->command('app:send-upcomming-dues-mail')->everyMinute();
+
+
+        $time = OrganizationSetting::where('key', 'auto_notify')
+            ->value('value'); // e.g. "15:01"
+
+        if ($time) {
+            $schedule->command('app:send-upcomming-dues-mail')
+                ->dailyAt($time)
+                ->withoutOverlapping();
+        }
     }
 
     /**
